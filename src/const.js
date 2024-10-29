8a5ace9a2dd913476c6b1448cab3324d9d1d23c9
Zoraxis <b.v.o0077@gmail.com>
2024-10-29 09:34:47 +0100

    a

--- src/const.js
+++ src/const.js
@@ -1,4 +1,4 @@
 export const contractAddress =
-  "kQCnWcXyNiERWSnVv2OwWsz97uynXjrb_dqrCrZCM0GNRE93";
+  "EQBOVIMkIM1Sdxs5ugIT5LWABir-vN3J7oxSad3TCfxC7bXO";
 export const mnemonic =
   "uphold number uncover grape bread beef result garage boil genuine jeans ocean sleep sign beauty pyramid measure olympic move wage garment bench ripple planet";
--- src/convert.js
+++ src/convert.js
@@ -1,3 +1,3 @@
 import { Address } from "@ton/ton";
 
-console.log(Address.parse("0:1753452c3b1012b0c353b613bf1c1b0cad4ade5aa0cf50fa06bb8d4e84e7c0fc").toString());
+console.log(Address.parse("0:e67476fcc662ba688e666a3312e7a5188ab53715a5ea1d703f25d5b356c4ea21").toString());
