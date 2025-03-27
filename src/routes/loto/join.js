463a0ff5baaeb05652514d44b49af544ba8a363c
Zoraxis <b.v.o0077@gmail.com>
2025-03-27 21:19:24 +0100

    address join

--- src/routes/loto/join.js
+++ src/routes/loto/join.js
@@ -1,12 +1,14 @@
 import { games, io } from "../../index.js";
 import dotenv from "dotenv";
 import { hideAddress } from "../../utils/hideAddress.js";
+import { Address } from "@ton/ton";
 dotenv.config();
 
 export const JoinedHandle = ({ gameId, address }) => {
   console.log(`user joined game FRFR`, address);
+  const addressFreindly = Address.parseRaw(address).toString();
   games[gameId].players.push({
-    address: hideAddress(address),
+    address: hideAddress(addressFreindly),
     timestamp: Date.now(),
   });
   games[gameId].prize += games[gameId].entry;
