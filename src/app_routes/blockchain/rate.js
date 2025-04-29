c239deee38f5b6bcd183be5fab815d81899a0a7a
Zoraxis <b.v.o0077@gmail.com>
2025-04-29 22:29:56 +0200

    rate send

--- src/app_routes/blockchain/rate.js
+++ src/app_routes/blockchain/rate.js
@@ -3,5 +3,5 @@ import { coin_rates } from "../../index.js";
 export const BlockchainCoinRate = async (req, res) => {
   const { coin } = req.params;
 
-  res.send(coin_rates[coin]);
+  res.send({rate: coin_rates[coin]});
 };
