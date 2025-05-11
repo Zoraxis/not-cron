496594ae5f41d9fca00e990ee0d808b97198344b
Zoraxis <b.v.o0077@gmail.com>
2025-05-11 17:10:07 +0200

    no en if not players

--- src/routes/utils/simulate_end.js
+++ src/routes/utils/simulate_end.js
@@ -3,6 +3,7 @@ import { end_all } from "../../lib/end_all.js";
 
 export const SimulateEnd = async (req, res) => {
   for (const game of Object.values(games)) {
+    if (game?.players?.length <= 0) continue;
     await end_all(game, 0);
   }
 
