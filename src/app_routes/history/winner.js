5439cadd8ef56a85aaae2b6c4a4e78ae86e8dcb2
Zoraxis <b.v.o0077@gmail.com>
2025-05-12 16:17:26 +0200

    winners return

--- src/app_routes/history/winner.js
+++ src/app_routes/history/winner.js
@@ -1,17 +1,22 @@
+import { ObjectId } from "mongodb";
 import { client } from "../../index.js";
+import { hideAddress } from "../../utils/hideAddress.js";
 
 export const HistoryWinner = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.query;
 
   const db = client.db("notto");
   const archive = db.collection("archive_games");
 
-  const winnersRaw = await archive
-    .findOne({
-      _id: new ObjectId(id),
-    })
-    .limit(1);
+  const winnersRaw = await archive.findOne({
+    _id: new ObjectId(id),
+  });
+  console.log(winnersRaw);
 
-  const winners = (await winnersRaw.toArray());
-  return res.send(winners);
+  return res.send(
+    winnersRaw?.players?.map((player) => ({
+      name: hideAddress(player.address),
+      date: player.date,
+    })) ?? []
+  );
 };
