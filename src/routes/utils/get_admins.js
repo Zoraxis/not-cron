4be62a94a95049c81c7036c97bd9d946047254b8
Zoraxis <b.v.o0077@gmail.com>
2025-05-18 19:42:44 +0200

    no client connect

--- src/routes/utils/get_admins.js
+++ src/routes/utils/get_admins.js
@@ -1,9 +1,6 @@
 import { client } from "../../index.js";
 
 export const GetAdmins = async (req, res) => {
-  const address = req.headers["x-user-adress"];
-  if (!address) return res.send({ message: "User not found", status: 400 });
-
   await client.connect();
   const db = client.db("notto");
 
