2954fc6a0f2d7da2d11fe68b794c1677ae451eca
Zoraxis <b.v.o0077@gmail.com>
2025-05-18 16:06:21 +0200

    fetch every 2

--- src/index.js
+++ src/index.js
@@ -172,7 +172,7 @@ app.post("/api/utils/multisig", ToggleMultisig);
 app.get("/api/utils/multisig", GetMultisig);
 app.get("/api/utils/multisig/address", GetMultisigAddress);
 
-cron.schedule("*/1 * * * * *", fetch_games);
+cron.schedule("*/2 * * * * *", fetch_games);
 cron.schedule(`*/10 * * * * *`, check_time);
 cron.schedule("*/6 * * * * *", check_transactions);
 cron.schedule("* * */3 * * *", fetch_rates);
